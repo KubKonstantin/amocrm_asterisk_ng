262df749e94f14682149e94bd16099ce0d6e4650
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2026-03-18 20:34:21 +0600

    Add /record-buffer/records route alias for record downloads

--- asterisk_ng/interfaces/domain/functions/IGetAgentCallQuery.py
+++ asterisk_ng/interfaces/domain/functions/IGetAgentCallQuery.py
@@ -1,5 +1,7 @@
 from asterisk_ng.system.dispatcher import IQuery
 
+from typing import Optional
+
 from ..models import CallDomainModel
 from ...crm_system import CrmUserId
 
@@ -7,9 +9,9 @@ from ...crm_system import CrmUserId
 __all__ = ["IGetAgentCallQuery"]
 
 
-class IGetAgentCallQuery(IQuery[CallDomainModel]):
+class IGetAgentCallQuery(IQuery[Optional[CallDomainModel]]):
 
     __slots__ = ()
 
-    async def __call__(self, agent_id: CrmUserId) -> CallDomainModel:
+    async def __call__(self, agent_id: CrmUserId) -> Optional[CallDomainModel]:
         raise NotImplementedError()
