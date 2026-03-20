1a8ccc253b1a20e06989b46c2038f46a3159daf7
KubKonstantin <123465354+KubKonstantin@users.noreply.github.com>
2026-03-20 18:52:12 +0600

    Use strict CDR uniqueid lookup for recordingfile

--- asterisk_ng/plugins/domain/functions/impl/GetAgentCallQueryImpl.py
+++ asterisk_ng/plugins/domain/functions/impl/GetAgentCallQueryImpl.py
@@ -1,11 +1,9 @@
-from typing import Mapping
-import logging
+from typing import Mapping, Optional
 
 from asterisk_ng.interfaces import CallDomainModel
 from asterisk_ng.interfaces import CrmUserId
 from asterisk_ng.interfaces import IGetAgentCallQuery
 
-logger = logging.getLogger(__name__)
 __all__ = ["GetAgentCallQueryImpl"]
 
 
@@ -18,7 +16,5 @@ class GetAgentCallQueryImpl(IGetAgentCallQuery):
     def __init__(self, active_calls: Mapping[CrmUserId, CallDomainModel]) -> None:
         self.__active_calls = active_calls
 
-    async def __call__(self, user_id: CrmUserId) -> CallDomainModel:
-        print("ACTIVE_CALLS KEYS:", list(self.__active_calls.keys()))
-        print("REQUESTED CRM USER:", user_id)
-        return self.__active_calls[user_id]
+    async def __call__(self, user_id: CrmUserId) -> Optional[CallDomainModel]:
+        return self.__active_calls.get(user_id)
